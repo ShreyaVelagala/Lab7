e0ec7bc1b333aed461b72d59d80a88925502a348
AchuthKrishna <92132574+AchuthKrishna@users.noreply.github.com>
2023-02-27 18:52:02 -0800

    Called getSynced and upsertSynced.

--- app/src/main/java/edu/ucsd/cse110/sharednotes/viewmodel/NoteViewModel.java
+++ app/src/main/java/edu/ucsd/cse110/sharednotes/viewmodel/NoteViewModel.java
@@ -30,11 +30,12 @@ public class NoteViewModel extends AndroidViewModel {
         if (note == null) {
             note = repo.getLocal(title);
         }
+        repo.getSynced(title);
         return note;
     }
 
     public void save(Note note) {
         // TODO: try to upload the note to the server.
-        repo.upsertLocal(note);
+        repo.upsertSynced(note);
     }
 }
